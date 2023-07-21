735d247c76cda2e1e29220d58f513873a4535dc6
Dhrumil Jigneshkumar Desai <200557507@ba-lc5-2407.student.georgianc.on.ca>
2023-07-21 10:28:35 -0400

    ice9 folder upload

---
+++ ICE9/ICE9/MovieTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  MovieTableViewCell.swift
+//  ICE9
+//
+//  Created by Dhrumil Jigneshkumar Desai on 2023-07-14.
+//
+
+import Foundation
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLabel: UITextField!
+    
+    @IBOutlet weak var studioLabel: UITextField!
+    
+    @IBOutlet weak var ratingLabel: UITextField!
+}
